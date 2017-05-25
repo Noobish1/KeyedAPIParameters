36aee75ae2473ed1669c6ca8178c24ad744177ee
Blair McArthur <blair.mcarthur@skipthedishes.com>
2017-05-25 11:17:55 -0500

    Initial Commit

---
+++ KeyedAPIParameters/KeyedAPIParameters.h
@@ -0,0 +1,19 @@
+//
+//  KeyedAPIParameters.h
+//  KeyedAPIParameters
+//
+//  Created by Blair McArthur on 25/05/17.
+//  Copyright © 2017 Noobish1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyedAPIParameters.
+FOUNDATION_EXPORT double KeyedAPIParametersVersionNumber;
+
+//! Project version string for KeyedAPIParameters.
+FOUNDATION_EXPORT const unsigned char KeyedAPIParametersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyedAPIParameters/PublicHeader.h>
+
+
