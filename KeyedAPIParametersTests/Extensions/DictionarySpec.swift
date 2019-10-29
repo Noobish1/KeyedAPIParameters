aa544ef48c305f704d627fbbfc39e5da59e12d50
Blair McArthur <blair.mcarthur@icloud.com>
2019-10-29 15:45:52 -0400

    Removed the mapValues test.

--- KeyedAPIParametersTests/Extensions/DictionarySpec.swift
+++ KeyedAPIParametersTests/Extensions/DictionarySpec.swift
@@ -15,16 +15,6 @@ class DictionarySpec: QuickSpec {
                     expect(actualKeys.count) == 2
                 }
             }
-            
-            describe("mapValues") {
-                it("should map the values") {
-                    let dict: [String : Int] = ["one" : 1]
-                    let expectedValues = [1]
-                    let actualValues = Array(dict.mapValues { $0 }.values)
-                    
-                    expect(actualValues) == expectedValues
-                }
-            }
         }
     }
 }
